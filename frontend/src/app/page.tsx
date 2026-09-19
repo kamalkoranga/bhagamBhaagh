2be95a4c8ee25a40e85a9eb413e55ee2e6b75cd2
klka <83089446+kamalkoranga@users.noreply.github.com>
2026-09-19 15:18:33 +0530

    redirect to dashboard bydefault + wrapped children with authprovider

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,8 +1,5 @@
 import { redirect } from 'next/navigation';
 
 export default function Home() {
-  // redirect('/dashboard');
-  return <>
-    <h1>Hello World!</h1>
-  </>;
+  redirect('/dashboard');
 }
